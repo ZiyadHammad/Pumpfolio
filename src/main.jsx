65925beb856bd3d7fd14bde4a3121d6b872e9d6a
Ziyad Hammad <ziyadhammad0@gmail.com>
2024-08-02 20:54:49 -0400

    Configured tailwind and added a layout

--- src/main.jsx
+++ src/main.jsx
@@ -8,16 +8,23 @@ import ExerciseDetail from "./pages/ExerciseDetail";
 
 // Styling
 import "./global.css";
+import MainLayout from "./layouts/MainLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/exercises/:id",
-    element: <ExerciseDetail />,
-  },
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: 'exercises/:id',
+        element: <ExerciseDetail />
+      }
+    ]
+    }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className='flex h-[100vh] items-center justify-center'>
-      <h1 className="text-8xl text-purple-500 font-bold" >Home <span className="text-blue-500" >Page</span></h1>
+      <h1 className="text-8xl text-primary font-bold" >Home <span className="text-blue-500" >Page</span></h1>
     </div>
   )
 }
