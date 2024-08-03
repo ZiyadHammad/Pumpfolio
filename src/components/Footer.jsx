cff528823799dbad70596f1651cefce7aa6cc634
Ziyad Hammad <ziyadhammad0@gmail.com>
2024-08-03 00:09:43 -0400

    Home

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-green-200 p-4">
+    <footer className="bg-[#161921] p-4">
       <Link to="/" className="flex flex-col justify-center items-center">
         <img src="/logo.png" alt="logo" className="h-10 w-10" />
         <h2>pumpfolio.</h2>
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,12 @@
 import { Outlet } from "react-router-dom";
 
-import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
-    <div className="bg-theme font-roboto">
+    <div className="bg-[#000] font-roboto">
       <div className="max-w-[1400px] mx-auto">
-        <Navbar />
-        <div className="p-4">
+        <div className="p-4 bg-[#0F1117]">
           <Outlet />
         </div>
         <Footer />
