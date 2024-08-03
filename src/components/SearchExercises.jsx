3e81147d0a57629d9e412bf736da816828ca8a15
Ziyad Hammad <ziyadhammad0@gmail.com>
2024-08-03 17:26:07 -0400

    Home

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,24 +1,32 @@
 import { useEffect, useState } from "react";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 
-const SearchExercises = () => {
+const SearchExercises = ({setExercises}) => {
+  const [search, setSearch] = useState('')
+
   const handleSearch = () => {};
 
+  // useEffect(() => {
+
+  // }, [])
+
   return (
-    <section className="flex flex-col mt-10">
-      <h1>Search Exercises By Category</h1>
-      <div className="">
+    <section className="flex flex-col">
+      <h1 className="text-xl ">Search Exercises By Category</h1>
+      <div className="flex gap-1">
         <input
           type="search"
           placeholder="Search for Exercises..."
-          className="bg-white h-[30px] w-[280px] rounded-lg"
+          className="bg-white h-[30px] w-[280px] rounded-lg pl-2"
           onChange={() => {}}
         />
-        <button className="rounded bg-pink-600" onClick={() => {}}>
+        <button
+          className="rounded-lg bg-pink-600 cursor-pointer p-1 text-md "
+          onClick={() => {}}
+        >
           Search
         </button>
       </div>
-      <div className=""></div>
     </section>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react";
 
-import Hero from '../components/Hero'
-import SearchExercises from '../components/SearchExercises'
-import Exercises from '../components/Exercises'
+import Hero from "../components/Hero";
+import SearchExercises from "../components/SearchExercises";
+import Exercises from "../components/Exercises";
 
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodypart, setBodyPart] = useState("all");
+  
   return (
     <>
       <Hero />
       <SearchExercises />
       <Exercises />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
